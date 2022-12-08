89f96a6040aaacc1813ee9a2cab93d6e9691d506
ayeseeem <ayeseeem@gmail.com>
2022-12-08 17:20:32 +0000

    build: convert from Python 2 to Python 3
    Changes applied automatically using 2to3. Used -d to update the
    doctests.
     It only changed some leading zeros, and changed print statements to
    print() calls.
    
    The leading zeros had just been to keep dates in a 2-digit style, so
    it's better to remove the leading 0 than add an o, so had to change
    those manually.

--- cigs.py
+++ cigs.py
@@ -3,5 +3,5 @@ from anniversary import celebrate
 
 if __name__ == "__main__":
 
-    quit_datetime = datetime.combine(date(2009, 11, 03), time(8, 30))
+    quit_datetime = datetime.combine(date(2009, 11, 3), time(8, 30))
     celebrate(quit_datetime)
